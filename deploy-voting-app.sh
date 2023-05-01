6b9e7c088029dee493ea8b3098b0a22d45ce26f4
abdisamadHussein <abdisamadhuseein@gmail.com>
2023-05-01 10:54:35 +0300

    final commit

--- deploy-voting-app.sh
+++ deploy-voting-app.sh
@@ -5,39 +5,39 @@ git clone  https://github.com/abdisamadHussein/deploying-voting-app-on-kubernete
 kubectl apply -f  vote-namespace.yaml
 
 ### db-deployment
-kubectl apply -f db-deployment.yml
+kubectl apply -f db-deployment.yaml
 
 ### db-service
 
-kubectl apply -f db-service.yml
+kubectl apply -f db-service.yaml
 
 ### redis-deployment
 
-kubectl apply -f redis-deployment.yml
+kubectl apply -f redis-deployment.yaml
 
 ### redis-service
 
-kubectl apply -f redis-serivce.yml
+kubectl apply -f redis-serivce.yaml
 
 ### worker-deployment
 
-kubectl apply -f worker.yml
+kubectl apply -f worker.yaml
 
 ### result-deployment
 
-kubectl apply -f result-deployment.yml
+kubectl apply -f result-deployment.yaml
 
 ### result-service
 
-kubectl apply -f result-service.yml
+kubectl apply -f result-service.yaml
 
 ### vote-deployment
 
-kubectl apply -f vote-deployment.yml
+kubectl apply -f vote-deployment.yaml
 
 ### vote-service
 
-kubectl apply -f vote-service.yml
+kubectl apply -f vote-service.yaml
 
 ### validate 
 kubectl get all -n vote 
